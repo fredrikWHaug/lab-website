4b7d5ffd0d11cd440b168964b301ee8e2405ef51
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2025-01-04 21:52:00 +0100

    Set up Flask-Talisman and resolved environment issues

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,11 @@
-from flask import flask
+from flask import Flask
 from flask_cors import CORS
+from flask_talisman import Talisman
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
+    Talisman(app)  # Enforce secure headers
 
     from .routes import api
     app.register_blueprint(api)
